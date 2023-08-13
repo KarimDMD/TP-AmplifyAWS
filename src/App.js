965256e6ff35acc5822e5db88e99de67b4bf30bc
KarimDMD <karim.damad.p@hotmail.com>
2023-08-13 18:32:42 +0200

    fix issue with styles.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Amplify, Storage, API } from "aws-amplify";
 import { withAuthenticator, Button } from "@aws-amplify/ui-react";
-// import "@aws-amplify/ui-react/styles.css";
+// eslint-disable-next-line import/no-unresolved
+import "@aws-amplify/ui-react/styles.css";
 import awsExports from "./aws-exports";
 import { useEffect, useState } from "react";
 import {
